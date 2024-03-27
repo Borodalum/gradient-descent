69063af94c7dd88677fae82f64daeadffe6934c0
brrdlam <luchek7331@gmail.com>
2024-03-27 13:26:38 +0300

    Fixed naming and other small issues

--- gradient_descent.py
+++ gradient_descent.py
@@ -10,14 +10,12 @@ def gradient_descent(f, gradient, x0, y0, learnig_rate, epsilon=1e-6, num_iterat
         grad = gradient(x_k, y_k)
         x_k1 = x_k - learnig_rate * grad[0]
         y_k1 = y_k - learnig_rate * grad[1]
-        f_k1 = f(x_k1, y_k1)
-        delta_f = abs(f_k1 - f(x_k, y_k))
 
-        if delta_f < epsilon:
+        if abs(f(x_k1, y_k1) - f(x_k, y_k)) < epsilon:
             break
         x_k, y_k = x_k1, y_k1
 
     end_time = time.time()
     execution_time = end_time - start_time
 
-    return x_k1, y_k1, f_k1, i, execution_time
+    return x_k1, y_k1, i, execution_time
--- main.py
+++ main.py
@@ -25,24 +25,24 @@ x0 = 1
 y0 = 1
 
 # Коэффициент
-a_k = 0.1
+learning_rate = 0.1
 
 # Вызов функции градиентного спуска для функции x^2 * y^2
-x_opt1, y_opt1, f_opt1, num_iterations1, execution_time1 = gradient_descent(f1, gradient1, x0, y0, a_k)
+x_opt1, y_opt1, num_iterations1, execution_time1 = gradient_descent(f1, gradient1, x0, y0, learning_rate)
 
 print("Функция x^2 * y^2:")
 print(f"Критерий останова: |delta f| < 1e-8")
 print(f"Число итераций: {num_iterations1}")
 print(f"Полученная точка: ({x_opt1}, {y_opt1})")
-print(f"Полученное значение функции: {f_opt1}")
+print(f"Полученное значение функции: {f1(x_opt1, y_opt1)}")
 print(f"Время работы: {execution_time1:.4f} сек")
 
 # Вызов функции градиентного спуска для функции x^2 + y^2
-x_opt2, y_opt2, f_opt2, num_iterations2, execution_time2 = gradient_descent(f2, gradient2, x0, y0, a_k)
+x_opt2, y_opt2, num_iterations2, execution_time2 = gradient_descent(f2, gradient2, x0, y0, learning_rate)
 
 print("\nФункция x^2 + y^2:")
 print(f"Критерий останова: |delta f| < 1e-8")
 print(f"Число итераций: {num_iterations2}")
 print(f"Полученная точка: ({x_opt2}, {y_opt2})")
-print(f"Полученное значение функции: {f_opt2}")
+print(f"Полученное значение функции: {f2(x_opt2, y_opt2)}")
 print(f"Время работы: {execution_time2:.4f} сек")
