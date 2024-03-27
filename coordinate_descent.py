47efdb2c630445889e26feba18353f95973bb807
brrdlam <luchek7331@gmail.com>
2024-03-27 18:22:50 +0300

    Pretty fixes

--- coordinate_descent.py
+++ coordinate_descent.py
@@ -2,7 +2,7 @@ import time
 
 from golden_section_search import golden_section_search
 
-def coordinate_descent(f, gradient, x0, y0, learning_rate=golden_section_search, epsilon=1e-12, num_iterations=1000):
+def coordinate_descent(f, gradient, x0, y0, learning_rate=golden_section_search, epsilon=1e-8, num_iterations=1000):
     x_prev = x0
     y_prev = y0
 
@@ -13,18 +13,16 @@ def coordinate_descent(f, gradient, x0, y0, learning_rate=golden_section_search,
         grad = gradient(x_prev, y_prev)
 
         # Оптимизация learning rate для каждого направления
-        learning_rate_x = learning_rate(f, x_prev, y_prev, grad, epsilon)
-        learning_rate_y = learning_rate(f, x_prev, y_prev, grad, epsilon)
-
+        learning_rate_val = learning_rate(f, x_prev, y_prev, grad, epsilon)
         i += 1
 
         # Шаг покоординатного спуска с использованием оптимальных learning rate
-        x = x_prev - learning_rate_x * grad[0]
+        x = x_prev - learning_rate_val * grad[0]
         if abs(f(x, y_prev) - f(x_prev, y_prev)) < epsilon:
             break
         x_prev = x
 
-        y = y_prev - learning_rate_y * grad[1]
+        y = y_prev - learning_rate_val * grad[1]
         if abs(f(x_prev, y) - f(x_prev, y_prev)) < epsilon:
             break
         y_prev = y
